a41dd162c67576702716522cedb098806958e657
keller77 <keller77@purdue.edu>
2018-11-28 17:21:33 -0500

    converted owner_dash
    converted owner_dash file to UTF8 without BOM. should get riid of errors

--- site/owner_dash.php
+++ site/owner_dash.php
@@ -1,4 +1,4 @@
-﻿<?php session_start();?>
+<?php session_start();?>
  <?php
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
