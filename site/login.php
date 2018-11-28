029ba745417a41321e1af773caa0a06a1879ec81
keller77 <keller77@purdue.edu>
2018-11-28 09:09:47 -0500

    Login page update
    Updated the login page(should pass variable $id, $username to whatever you need it to owner and lessee dashboards )

--- site/login.php
+++ site/login.php
@@ -1,5 +1,100 @@
 <?php
-session_start()
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: welcome.php");
+  exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ $sub = $_POST["Submit"];
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+		// Prepare a select statement
+		if($sub == "OwnerLogin"){
+        $sql = "SELECT ID, Email, Password FROM Owner FROM Owner WHERE Email = ?";
+		} else {
+			$sql = "SELECT ID, Email, Password FROM Owner FROM Lessee WHERE Email = ?";
+		}
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $sqlpassword);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if($password == $sqlpassword){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+							if($sub == "OwnerLogin"){
+								header("location: owner_dash.php");
+							} else if($sub == "LesseeLogin"){
+								header("location: lessee_dash.php");
+							}
+                        } else {
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
 ?>
 <!DOCTYPE HTML>
 <!--
@@ -14,162 +109,70 @@ session_start()
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
-<body>
-	<!-- Header -->
-		<header id="header">
-			<a href="index.html" class="logo"><strong>WARIE</strong> &ensp; Home</a>
-			<nav>
-				<a href="#menu">Menu</a>
+	<body>
+		<!-- Header -->
+			<header id="header">
+				<a href="index.html" class="logo"><strong>WARIE</strong> &ensp; Home</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="browse.html">Browse Warehouses</a></li>
+					<li><a href="lessees.html">Lease a warehouse</a></li>
+					<li><a href="owners.html">List your warehouse</a></li>
+					<li><a href="login.html">Login</a></li>
+				</ul>
 			</nav>
-		</header>
 
-	<!-- Nav -->
-		<nav id="menu">
-			<ul class="links">
-				<li><a href="index.html">Home</a></li>
-				<li><a href="browse.html">Browse Warehouses</a></li>
-				<li><a href="lessees.html">Lease a warehouse</a></li>
-				<li><a href="owners.html">List your warehouse</a></li>
-				<li><a href="login.html">Login</a></li>
-			</ul>
-		</nav>
-
-	<!--Banner-->
-	<section class="banner_layout banner_login">
-			<div class="inner">
-			</br></br></br>
-				<h1><font color="white">Login with WARIE</font></h1></br>
-			</div>
-	</section>
-	
-	<!-- PHP code -->
-	<?php
-	   define('DB_SERVER', 'mydb.ics.purdue.edu');
-	   define('DB_USERNAME', 'g1090423');
-	   define('DB_PASSWORD', 'marioboys');
-	   define('DB_DATABASE', 'g1090423');
-	   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-	?>
-	<?php 
-	$_SESSION["err"] = "";
-	   if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Submit"])) {
-			
-		  // username and password sent from form 
-		  $sub = $_POST["Submit"];
-		  $myemail = mysqli_real_escape_string($db,$_POST["email"]);
-		  $mypassword = mysqli_real_escape_string($db,$_POST["password"]); 
-
-		  //if (isset($sub["OwnerLogin"])) {
-		  if ($sub == "OwnerLogin") {
-		  $sql = "SELECT ID,FirstName FROM Owner O WHERE O.Email = $myemail and O.Password = $mypassword";
-		  //$result = mysqli_query($db,$sql);
-		  $result = mysqli_query($db,"SELECT ID,FirstName FROM Owner O WHERE O.Email = '".$myemail."' and O.Password = '".$mypassword."'");
-		  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		  $active = $row['active'];
-		  $count = mysqli_num_rows($result);
-		  $_SESSION["ID"] = $row[0][0];
-		  }
-		  // If result matched $myusername and $mypassword, table row must be 1 row
-			//elseif (isset($sub["LesseeLogin"])){
-			elseif ($sub == "LesseeLogin"){
-			$sql = "SELECT ID FROM Lessee L WHERE L.Email = '".$myemail."' and L.Password = '".$mypassword."'";
-			$result = mysqli_query($db,$sql);
-			$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-			$active = $row['active'];
-			$count = mysqli_num_rows($result);
-			}
-		
-			if($count == 1) {
-			$_SESSION["login"] = "1";
-			$_SESSION["email"] = $myemail;
-			//session_register("$myemail");
-			//if(isset($sub["OwnerLogin"])){
-			if($sub == "OwnerLogin"){
-				echo '<script>window.location.assign("http://web.ics.purdue.edu/~g1090423/owners.html");</script>';
-				exit();
-			}
-			else {
-				echo '<script>window.location.assign("http://web.ics.purdue.edu/~g1090423/lessees.html");</script>';
-				exit();
-			}
-		}elseif(!empty($_POST["email"]) && !empty($_POST["password"])){	
-			 $_SESSION["err"] = "* Your Email or Password is invalid";
-		  }
-		}
-	mysqli_close($db);
-	?>	
-<!-- Forms and stuff -->
-	<section id="main">
-		<div class="slimmer">
-		<h3>View or update account information, listings, or contracts</h3>
-		<br/><br/>
-			<h4>Credentials</h4>
-			<h5><span style="color:red"><?php echo $_SESSION["err"];?></span></h5>
-		   <form method = "post">
-			<div class="6u$ 12u$(small)">
-			  <label>Email  :</label><input type = "email" name = "email" class = "box" value = "awilson@gmail.com"/>
-			  <label>Password  :</label><input type = "password" name = "password" class = "box" value = "6n1yGl%!o2"/><br/>
-			</div>
-			  <input type = "submit" name = "Submit" value ="LesseeLogin" class="button special"/>&emsp;&emsp;<br /><br />
-			  <input type = "submit" name = "Submit" value = "OwnerLogin" class="button special"/>&emsp;&emsp;<br />
-		   </form>
-		   
-		</div>
-	</section>	
-	
-	<!-- Footer -->
-	<footer id="footer">
-		<div class="copyright">
-			&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
-		</div>
-	</footer>
-
-<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/jquery.scrolly.min.js"></script>
-	<script src="assets/js/skel.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<script src="assets/js/main.js"></script>	
-
-
-		<!-- Main 
-			<section id="main">
-				<div class="slimmer">
-					<h3>View or update account information, listings, or contracts</h3>
-					<!----<form method="post" action="#">
-						<div class="row uniform 50%">
-							<div class="12u$"><!--
-								<div class="select-wrapper">
-									<h4>Select an action:</h4>
-									<select name="category" id="category">
-										<option value="">- User -</option>
-										<option value="1">New Lesee</option>
-										<option value="2">New Owner</option>
-										<option value="3">Lesee Login</option>
-										<option value="3">Owner Login</option>
-									</select> 
-								</div>
-								
-								</br>
-								<h4>Credentials</h4>
-								<div class="6u$ 12u$(small)">
-									<input type="email" name="email" id="email" value="" placeholder="Email" />
-								</div>
-								</br>
-								<div class="6u$ 12u$(small)">
-									<input type="text" name="password" id="password" value="" placeholder="Password" />
-								</div>
-							</div>
-						</div>
-					</form> 
-					<a href="login.html" class="button special">Lessee Login</a>
-					&emsp;&emsp;
-					<a href="login.html" class="button special">Owner Login</a>
+		<!--Banner-->
+		<section class="banner_layout banner_login">
+				<div class="inner">
+				</br></br></br>
+					<h1><font color="white">Login with WARIE</font></h1></br>
 				</div>
-			</br></br></br>
-			</section>-->
+		</section>
+	<!-- Forms and stuff -->
+		<section id="main">
+			<div class="slimmer">
+			<h3>View or update account information, listings, or contracts</h3>
+			<br/><br/>
+				<h4>Credentials</h4>
+				<h5><span style="color:red"><?php echo $_SESSION["err"];?></span></h5>
+				
+			   <form method = "post">
+				<div class="6u$ 12u$(small)">
+					<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+						<label>Email  :</label><input type = "email" name = "email" class = "box" value="<?php echo $username; ?>"> 
+						<span class="help-block"><?php echo $username_err; ?></span>
+					</div> 
+					<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+						<label>Password  :</label><input type = "password" name = "password" class = "box" value = "6n1yGl%!o2">
+						<span class="help-block"><?php echo $password_err; ?></span><br/>
+					</div>
+				</div>
+				  <input type = "submit" name = "Submit" value ="LesseeLogin" class="button special">&emsp;&emsp;<br /><br />
+				  <input type = "submit" name = "Submit" value = "OwnerLogin" class="button special">&emsp;&emsp;<br />
+			   </form>
+			</div>
+		</section>	
+		
+		<!-- Footer -->
+		<footer id="footer">
+			<div class="copyright">
+				&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
+			</div>
+		</footer>
 
-			
-
+	<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.scrolly.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>	
 	</body>
 </html>
