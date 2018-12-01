70b5d660d7b09d0b69589f3c5afed19adbfdfe41
sallyclemmons <44778617+sallyclemmons@users.noreply.github.com>
2018-12-01 12:41:06 -0500

    reverting back to old file
    fixing the weird merge errors

--- site/results.php
+++ site/results.php
@@ -20,7 +20,6 @@
 				<a href="index.html" class="logo"><strong>WARIE</strong> &ensp; Home</a>
 				<nav>
 					<a href="#menu">Menu</a>
-					<a href="about.html">About</a>
 				</nav>
 			</header>
 
@@ -46,26 +45,11 @@
 		<!-- Main -->
 			<section id="main">
 				<div class="slimmer">
-<<<<<<< HEAD
-					<header>
-						<h2>Search Results</h2>
-					</header>
-					<p><font color="black">
-						</br>
-						Click on any Warehouse ID to draft up a contract for that location.
-					</font></p>
-
-
-
-
-					<?php
-=======
 					<header><h2>Search Results</h2></header>
 					<p><font color="black"></br>Click on any Warehouse ID to draft up a contract for that location.</font></p>
-
-					<?php
+					
+					<?php 
 					//Getting all inputted search parameters and setting values for DB connection
->>>>>>> a98533b6c6390b87272b3e25acc13fdf53149bd3
 					$storage_type = $_POST["storagetype"];
 					$city = $_POST["city"];
 					$start_date = $_POST["start_date"];
@@ -76,77 +60,41 @@
 					$username = "g1090423";
 					$password = "marioboys";
 					$dbname = "g1090423";
-<<<<<<< HEAD
-
-					// Create connection
-=======
-
-
+					
+					
 					//Create connection
->>>>>>> a98533b6c6390b87272b3e25acc13fdf53149bd3
 					$conn = new mysqli($servername, $username, $password, $dbname);
 					//Check connection
 					if ($conn->connect_error) {
-<<<<<<< HEAD
-					die("Connection failed: " . $conn->connect_error);
-					}
-					$start_date1= date_create("$start_date");
-					$end_date1=date_create("$end_date");
-					$current_date = new DateTime(date("Y-m-d"));
-
-=======
 						die("Connection failed: " . $conn->connect_error);
-						}
-
+						} 
+						
 					//Get user-selected dates in correct format
 					$start_date1= date_create("$start_date");
 					$end_date1=date_create("$end_date");
 					$current_date = new DateTime(date("Y-m-d"));
-
+					
 					//Get user-selected dates in a format of "weeks out" (required for the search qry)
->>>>>>> a98533b6c6390b87272b3e25acc13fdf53149bd3
 					$start_date_week = ceil((date_diff($start_date1, $current_date))/7);
 					$end_date_week = ceil((date_diff($end_date1, $current_date))/7);
-
+					
 					//Create query that will return all warehouses in the correct location, with the correct storage type, that have enough availability between the weeks the user has searched for
 					$sql_1 = "SELECT W.ID, StorageCapacity,BasePrice,Zipcode,City,State,Owner_ID,R.Rating as Owner_Rating ";
 					$sql_2 = "FROM (Warehouse W INNER JOIN(SELECT MIN(Open_Space),WarehouseID FROM Availability WHERE WeekFromDate BETWEEN ".$start_date_week." AND ".$end_date_week." GROUP BY WarehouseID) A ";
 					$sql_3 = "ON W.ID = A.WarehouseID) INNER JOIN (SELECT Rating,Owner.ID FROM Owner) R ON W.Owner_ID=R.ID WHERE StorageType = ".$storage_type." AND City = '".$city."' ORDER BY ";
 					$sql = $sql_1.$sql_2.$sql_3;
-<<<<<<< HEAD
-
 					if($sort_val==1){$sql=$sql."BasePrice";};
 					if($sort_val==2){$sql=$sql."Owner_Rating DESC";};
-
-					$result = $conn->query($sql);
-					echo "<style>td,tr:hover{opacity:0.6;} th:hover{opacity:1.0;}</style>";
-					echo"<table width = 950px>";
-					echo"<th>Warehouse ID</th><th>Storage Capacity</th><th>Price ($/sq ft/month)</th><th>Zipcode</th><th>City</th><th>State</th><th>Owner ID</th><th>Owner Rating</th>";
-
-					if ($result->num_rows > 0) {
-					// output data of each row
-					while($row = $result->fetch_assoc()) {
-
-					echo"<tr><td><a href='request.php?w=".$row['ID']."&o=".$row['Owner_ID']."&pr=".$row['BasePrice']."&c=".$row['City']."&st=".$row['State']."&z=".$row["Zipcode"]."' target='_blank'>".$row['ID']. "</a></td><td>".$row["StorageCapacity"]."</td><td>".round($row["BasePrice"],2)."</td><td>".$row["Zipcode"]."</td><td>".$row["City"]."</td><td>".$row["State"]."</td><td>".$row["Owner_ID"]."</td><td>".$row["Owner_Rating"]."</td></tr>";
-					//echo "ID: " . $row["ID"]. "Capacity: ".$row["Capacity"]. "Price: ".$row["Price"]. "Zipcode: ".$row["Zipcode"] ."City: ".$row["City"]. "State: ".$row["State"]. "Owner ID: ".$row["Owner_ID"]. "Owner Rating: ".$row["Owner_Rating"];
-
-					echo"</td>";
-						}
-						} else {
-							echo "0 results";
-=======
-					if($sort_val==1){$sql=$sql."BasePrice";};
-					if($sort_val==2){$sql=$sql."Owner_Rating DESC";};
-
+					
 					//Get result of the above query
 					$result = $conn->query($sql);
-
+					
 					//Start of the results table
 					echo "<style>td,tr:hover{opacity:0.6;} th:hover{opacity:1.0;}</style>";
 					echo"<table width = 950px>";
 					echo"<th>Warehouse ID</th><th>Storage Capacity</th><th>Price ($/sq ft/month)</th><th>Zipcode</th><th>City</th><th>State</th><th>Owner ID</th><th>Owner Rating</th>";
-
-					//If statement and while loop print all results returned from the above query
+					
+					//If statement and while loop print all results returned from the above query 
 					if ($result->num_rows > 0) {
 						while($row = $result->fetch_assoc()) {
 							//Warehouse ID appears as a link, which sends the user to a drafted contract for the user's inputted search parameters and selected warehouse information
@@ -156,39 +104,28 @@
 						} else {
 							//showing the user if there are no relevant results
 							echo "<h3>0 results</h3>";
->>>>>>> a98533b6c6390b87272b3e25acc13fdf53149bd3
 							}
 							//close connection
 							$conn->close();
 					//End table
 					echo"</table>";
-<<<<<<< HEAD
-
-					?>
-					</font></p>
-
-
-
-=======
 					?>
 					</font></p>
 					<!-- Restart search -->
->>>>>>> a98533b6c6390b87272b3e25acc13fdf53149bd3
 					<a href="browse.html" class="button special" target="_blank">Search Again</a>
 				</div>
 			</section>
 
 		<!-- Footer -->
 			<footer id="footer">
-				 <ul class="icons">
+			<ul class="icons">
 					<li><a href="https://twitter.com/WARIE49834226" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 					<li><a href="https://www.facebook.com/WARIE-639800186472059/?modal=admin_todo_tour" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 					<li><a href="https://www.instagram.com/warie_business/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
 				</ul>
-				<a href ="terms_conditions.html">Terms and Conditions</a><br><br>
-
-				<div class="copyright" style="font-weight:300; font-size: 10px;">
-					&copy; Untitled. Design: <a href="https://templated.co" style="font-weight:300;">TEMPLATED</a>. Images: <a href="https://unsplash.com" style="font-weight:300;">Unsplash</a>.
+				<a href ="terms_conditions.html">Terms and Conditions</a>	
+				<div class="copyright">
+					&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
 				</div>
 			</footer>
 
