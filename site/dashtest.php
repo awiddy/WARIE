63f28ea3c91ce140d9f6be31fdd1961fa5727cd9
Oliver Schroeder <Oliver@Olivers-MacBook-Pro.local>
2018-12-01 10:39:06 -0500

    Updated with some learned info on how to store SQL queries as arrays in PHP
    I will now be able to math operations on the information from our data base during the meeting today

--- site/dashtest.php
+++ site/dashtest.php
@@ -22,40 +22,54 @@
 
         /*$contracts_sql = "SELECT * FROM Contract WHERE Owner_ID=".$Owner_ID." AND Approval=1";*/
         $contracts_sql = "SELECT * FROM Contract WHERE Owner_ID=5 AND Approval=1";
-        $contracts_result = $conn->query($contracts_sql);
-        
-
-        $contracts = array();
-        $contracts['cols'] = array(
-            array('lable' => "StartDate", 'type'=>'string'),
-            array('lable' => "EndDate", 'type'=>'string'),
-            array('lable' => 'ID', 'type' => 'string'),
-            array('lable' => "Lessee_Rating", 'type'=>'number'),
-            array('lable' => "Owner_Rating", 'type'=>'number'),
-            array('lable' => "Lessee_ID", 'type'=>'number'),
-            array('lable' => "Owner_ID", 'type'=>'number'),
-            array('lable' => "Rented_Space", 'type'=>'number'),
-            array('lable' => "SigningDate", 'type'=>'string'),
-            array('lable' => "Approval", 'type'=>'number'),
-        );
-
-        $contracts_rows = array();
-        while($row = $contracts_result->fetch_assoc()){
-            $temp = array();
-            $temp[] = array('v' => (string) $row['StartDate']);
-            $temp[] = array('v' => (string) $row['EndDate']);
-            $temp[] = array('v' => (int) $row['ID']);
-            $temp[] = array('v' => (int) $row['Lessee_Rating']);
-            $temp[] = array('v' => (int) $row['Owner_Rating']);
-            $temp[] = array('v' => (int) $row['Lessee_ID']);
-            $temp[] = array('v' => (int) $row['Owner_ID']);
-            $temp[] = array('v' => (int) $row['Rented_Space']);
-            $temp[] = array('v' => (string) $row['SigningDate']);
-            $temp[] = array('v' => (int) $row['Approval']);
+        $result = mysqli_query($conn, $contracts_sql);
+        $data = array();
+        if(mysqli_num_rows($result)>0){
+            while($row = mysqli_fetch_assoc($result)){
+                $data[] = $row;
+            }
         }
-        $contracts['rows'] = $contracts_rows;
-        $jsonTable = json_encode($contracts);
-        echo $jsonTable;
+       
+
+       foreach ($data as $data1){
+           echo $data1['Start Date'];
+           echo "        ";
+       }
+
+
+
+
+        // $contracts = array();
+        // $contracts['cols'] = array(
+        //     array('lable' => "StartDate", 'type'=>'string'),
+        //     array('lable' => "EndDate", 'type'=>'string'),
+        //     array('lable' => 'ID', 'type' => 'string'),
+        //     array('lable' => "Lessee_Rating", 'type'=>'number'),
+        //     array('lable' => "Owner_Rating", 'type'=>'number'),
+        //     array('lable' => "Lessee_ID", 'type'=>'number'),
+        //     array('lable' => "Owner_ID", 'type'=>'number'),
+        //     array('lable' => "Rented_Space", 'type'=>'number'),
+        //     array('lable' => "SigningDate", 'type'=>'string'),
+        //     array('lable' => "Approval", 'type'=>'number'),
+        // );
+
+        // $contracts_rows = array();
+        // while($row = $contracts_result->fetch_assoc()){
+        //     $temp = array();
+        //     $temp[] = array('v' => (string) $row['StartDate']);
+        //     $temp[] = array('v' => (string) $row['EndDate']);
+        //     $temp[] = array('v' => (int) $row['ID']);
+        //     $temp[] = array('v' => (int) $row['Lessee_Rating']);
+        //     $temp[] = array('v' => (int) $row['Owner_Rating']);
+        //     $temp[] = array('v' => (int) $row['Lessee_ID']);
+        //     $temp[] = array('v' => (int) $row['Owner_ID']);
+        //     $temp[] = array('v' => (int) $row['Rented_Space']);
+        //     $temp[] = array('v' => (string) $row['SigningDate']);
+        //     $temp[] = array('v' => (int) $row['Approval']);
+        // }
+        // $contracts['rows'] = $contracts_rows;
+        // $jsonTable = json_encode($contracts);
+        // echo $jsonTable;
 
 
 
@@ -69,4 +83,3 @@
 </body>
 
 </html>
-
