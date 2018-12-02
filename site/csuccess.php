bb4c2d1af593e911509153a52461db3f90ef33a9
sallyclemmons <44778617+sallyclemmons@users.noreply.github.com>
2018-12-01 20:42:29 -0500

    changing file name for clarity
    also fixed the contract addition to include correct start/end date

---
+++ site/csuccess.php
@@ -0,0 +1,117 @@
+<?php session_start();
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+	header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE HTML>
+<!--
+	Binary by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Sent!</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+			</head>
+	<body>
+	<!-- Header -->
+			<header id="header">
+				<a href="index.html" class="logo"><strong>WARIE</strong> &ensp; Home</a>
+				<nav>
+					<a href="#menu">Menu</a>
+					<a href="about.html">About</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="browse.html">Browse Warehouses</a></li>
+					<li><a href="lessees.html">Lease a warehouse</a></li>
+					<li><a href="owners.html">List your warehouse</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</nav>
+
+		<!--Banner-->
+		<section class="banner_layout banner_login">
+				<div class="inner">
+				</br></br></br>
+					<h1><font color="white">Request sent!</font></h1></br>
+				</div>
+			</section>
+
+		<!-- Main -->
+			<section id="main">
+				<div class="slimmer">
+					<h3>Please wait until you hear back from the owner.</h3>
+					<a href="browse.html" class="button special" target="_blank">Search Again</a>
+					<?php
+					//Retreiving data from URL/Post function, as well as setting variables for the query/DB connection
+					$start_date_raw = $_GET["sd"];
+					$end_date_raw = $_GET["ed"];
+					$o_id = $_GET['o'];
+					$l_id = $_GET['l'];
+					$sn = $_GET['sn'];
+					$w_id = $_GET['w'];
+					$signing_date1=new DateTime(date("Y-m-d"));
+					$approval=0; //approval always starts as 0, because it starts as unaccepted/pending
+					
+					
+					$servername = "mydb.ics.purdue.edu";
+					$username = "g1090423";
+					$password = "marioboys";
+					$dbname = "g1090423";
+
+					//Create connection
+					$conn = new mysqli($servername, $username, $password, $dbname);
+					//Check connection
+					if ($conn->connect_error) {
+						die("Connection failed: " . $conn->connect_error);
+						}
+
+
+					//Ensuring dates are in correct format
+					$signing_date = $signing_date1->format('Y-m-d');
+					//$start_date=$start_date_raw->format('Y-m-d');
+					//$end_date=$end_date_raw->format('Y-m-d');
+
+					//Writing query to add requested contract into Contract table in DB
+					$sql = "INSERT INTO Contract (`Start Date`,`End Date`,Lessee_ID,Owner_ID,Rented_Space, Signing_date,Warehouse_ID,Approval) VALUES ('".$start_date_raw."','".$end_date_raw."',".$l_id.",".$o_id.",".$sn.",'".$signing_date."',".$w_id.",".$approval.")";
+					
+					mysqli_query($conn,$sql);
+					mysqli_close($conn);
+					?>
+
+					</div>
+					</section>
+
+					<!-- Footer -->
+						<footer id="footer">
+							 <ul class="icons">
+								<li><a href="https://twitter.com/WARIE49834226" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.facebook.com/WARIE-639800186472059/?modal=admin_todo_tour" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/warie_business/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							</ul>
+							<a href ="terms_conditions.html">Terms and Conditions</a><br><br>
+
+							<div class="copyright" style="font-weight:300; font-size: 10px;">
+								&copy; Untitled. Design: <a href="https://templated.co" style="font-weight:300;">TEMPLATED</a>. Images: <a href="https://unsplash.com" style="font-weight:300;">Unsplash</a>.
+							</div>
+						</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
