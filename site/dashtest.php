cc50110fe9b6485f0f1e0f09866b154ceb5e74b3
Oliver Schroeder <Oliver@Olivers-MacBook-Pro.local>
2018-12-03 17:52:40 -0500

    Began adding timeline

--- site/dashtest.php
+++ site/dashtest.php
@@ -13,6 +13,76 @@
         <tr>";
         space_filled();
         echo"<tr></table>";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
+        function map(){
+
+                /*Open Data base connection*/
+            $Owner_ID=$_SESSION["id"]; 
+            $servername = "mydb.ics.purdue.edu";
+            $username = "g1090423";
+            $password = "marioboys";
+            $dbname = "g1090423";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+        //$location_sql = "Select Warehouse.Zipcode, Warehouse.ID From Warehouse Where Owner_ID = ".$Owner_ID."";
+        $location_sql = "Select Warehouse.Zipcode, Warehouse.ID From Warehouse Where Owner_ID = 1";
+
+        //Makes multidimensional array of contracts_sql query result
+        $location_result = mysqli_query($conn, $location_sql);
+        $location = array();
+        if(mysqli_num_rows($location_result)>0){
+            while($row = mysqli_fetch_assoc($location_result)){
+                $location[] = $row;
+            }
+        } 
+        $location_table = array();
+        foreach ($location as $location1){
+            $location_table[] = array((string)$location1['Zipcode'], (int)$location1['ID']); 
+        }
+
+        $location_table = json_encode($location_table);
+        print_r($location_table);
+        
+            $conn->close();
+        
+
+            echo"<head>";
+            echo'<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>';
+            echo'<script type="text/javascript">
+            google.charts.load("current", {
+                "packages":["map"],
+                // Note: you will need to get a mapsApiKey for your project.
+                // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+                "mapsApiKey": "AIzaSyDi799MTv01g9mQ2C2p1km1v7v1bTIjs-Q"
+            });
+            google.charts.setOnLoadCallback(drawChart);
+            function drawChart() {
+                var data = new google.visualization.DataTable();
+                    data.addColumn(\'string\', \'zip\');
+                    data.addColumn(\'number\', \'Warehouse ID\');
+                    data.addRows('.$location_table.');
+        
+                var map = new google.visualization.Map(document.getElementById(\'map_div\'));
+                map.draw(data, {
+                showTooltip: true,
+                showInfoWindow: true
+                });
+            }
+        
+            </script>';
+        echo"</head>
+        
+
+            <div id=\"map_div\" style=\"width: 100%; height: 40%\"></div>";
+
+        }
+        
+        
+/////////////////////////////////////////////////////////////////////////////////////        
         function revenue(){
             /*Open Data base connection*/
             $Owner_ID=$_SESSION["id"]; 
@@ -106,6 +176,7 @@
             if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
             }
+            //Need to fix query below to be dynamic with page content before uploading
             //$space_sql = "Select Warehouse.ID, sum(Warehouse.StorageCapacity), sum(Contract.Rented_Space) From Warehouse INNER JOIN Contract ON Warehouse.ID = Contract.Warehouse_ID Where Warehouse.Owner_ID=".$Owner_ID." AND Approval = 1";
             $space_sql = "Select Warehouse.ID, (sum(Warehouse.StorageCapacity)- sum(Contract.Rented_Space)) as Open, sum(Contract.Rented_Space) as Rented_Space From Warehouse INNER JOIN Contract ON Warehouse.ID = Contract.Warehouse_ID Where Warehouse.Owner_ID=1 AND Approval = 1";
             
@@ -164,7 +235,44 @@
                 <div id=\"chart1_div\"></div>";
 
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+        function timeline(){
+            /*Open Data base connection*/
+            $Owner_ID=$_SESSION["id"]; 
+            $servername = "mydb.ics.purdue.edu";
+            $username = "g1090423";
+            $password = "marioboys";
+            $dbname = "g1090423";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+            //Add dynamic sql
+            $time_sql = "Select * FROM Contract Where Owner_ID = 1 and Approval=1";
+            
+            $result = mysqli_query($conn, $time_sql);
+            $space = array();
+            if(mysqli_num_rows($result)>0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $space[] = $row;
+                }
+            }
+            $space_table = array();
+            $open_total = 0;
+            foreach ($space as $space1){
+                $open_total = $open_total + $space1['Open'];
+                $space_table[] =array((string)$space1['ID'], (int)$space1['Rented_Space']); 
+            }
+            
+            $open_total = array("Open", (int)$open_total);
+            $space_table[] = $open_total;
+            
+            $space_table = json_encode($space_table);
+            
 
+        }
             
     ?>
   </body>
