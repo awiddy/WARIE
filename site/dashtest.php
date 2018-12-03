8ea2c68dca0dc032427ed775257e327a5de9130a
Oliver Schroeder <Oliver@Olivers-MacBook-Pro.local>
2018-12-02 19:51:17 -0500

    Update dashtest.php
    Fixed some of the commenting

--- site/dashtest.php
+++ site/dashtest.php
@@ -42,25 +42,23 @@
            $revenue_table[] = array($data1['ID'],(int)$revenue); 
         }
         $revenue_table = json_encode($revenue_table);
+        
+        
         $conn->close();
        
     ?>
 
   <head>
-    <!--Load the Ajax API-->
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script type="text/javascript">
 
-    // Load the Visualization API and the piechart package.
     google.load('visualization', '1', {'packages':['corechart']});
 
-    // Set a callback to run when the Google Visualization API is loaded.
     google.setOnLoadCallback(drawChart);
 
     function drawChart() {
 
-      // Create our data table out of JSON data loaded from server.
       var data = new google.visualization.DataTable();
             data.addColumn('string', 'ID');
             data.addColumn('number','Revenue');
@@ -71,17 +69,15 @@
           width: 800,
           height: 600
         };
-      // Instantiate and draw our chart, passing in some options.
-      // Do not forget to check your div ID
+    
       var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
       chart.draw(data, options);
     }
     </script>
-  </head>
-
-  <body>
-    <!--this is the div that will hold the pie chart-->
+ </head>
+  
     <div id="chart_div"></div>
+
   </body>
 </html>
     
