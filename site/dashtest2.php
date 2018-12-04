574268dac4b3538a3f4ef50ef343ab08bfe54b8f
Oliver Schroeder <Oliver@Olivers-MacBook-Pro.local>
2018-12-04 11:54:14 -0500

    dashtest2 can now display any chart we want!!
    I can now display our stuff as a dashboard on the site!!!! I will upload to our actual site after lunch.

--- site/dashtest2.php
+++ site/dashtest2.php
@@ -5,122 +5,284 @@
 
 <body>
     <p>
-        Test Google Dashboard
+        Test Google Dashboard Full
     </p>
 
     <?php
-        /*Open Database connection*/
-        $Owner_ID=$_SESSION["id"]; 
-        $servername = "mydb.ics.purdue.edu";
-        $username = "g1090423";
-        $password = "marioboys";
-        $dbname = "g1090423";
-        // Createconnection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Checkconnection
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
 
-        //$contracts_sql = "Select Contract.*, Warehouse.StorageCapacity, Warehouse.BasePrice, Warehouse.Zipcode From Contract INNER JOIN Warehouse ON Contract.Warehouse_ID = Warehouse.ID Where Contract.Owner_ID = ".$Owner_ID." AND Contract.Approval=1";
-        $contracts_sql = "Select Contract.*, Warehouse.StorageCapacity, Warehouse.BasePrice, Warehouse.Zipcode From Contract INNER JOIN Warehouse ON Contract.Warehouse_ID = Warehouse.ID Where Contract.Owner_ID = 1 AND Contract.Approval=1";
-        
-        //Makes multidimensional array of contracts_sql query result
-        $contracts_result = mysqli_query($conn, $contracts_sql);
-        $contract = array();
-        if(mysqli_num_rows($contracts_result)>0){
-            while($row = mysqli_fetch_assoc($contracts_result)){
-                $contract[] = $row;
+        dashboard();
+        echo"<table>
+        <tr>
+        <div id=\"map_div\" style=\"width: 100%; height: 40%\"></div>
+        </tr>
+        <tr>
+        <div id=\"rev_div\"></div>
+        </tr>
+        <tr>
+        <div id=\"Space_div\"></div>
+        </tr>
+        <div id=\"timeline\" style=\"height: 500px;\"></div>
+        </tr>
+        </table>";
+
+        ///Makes Dashboard////
+        function dashboard(){
+         
+        ////////Bring In Map Data///////////
+        /*Open Data base connection*/
+            $Owner_ID=$_SESSION["id"]; 
+            $servername = "mydb.ics.purdue.edu";
+            $username = "g1090423";
+            $password = "marioboys";
+            $dbname = "g1090423";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
             }
-        }
-       
-		//Construct Revenue Table
-	   $revenue_table = array();
-       foreach ($contract as $contract1){
-		   $diff = strtotime($contract1['End Date']) - strtotime($contract1['Start Date']);   //Site the Stack overflow
-		   $diffDays = floor($diff/(3600*24));
-           $revenue = $contract1['BasePrice']/365 *$contract1['Rented_Space'] * $diffDays;
-           $revenue_table[] = array($contract1['ID'],(int)$revenue); 
-        }
-        //makes json file of revenue table
-        $revenue_table = json_encode($revenue_table);
-       
+            //$location_sql = "Select Warehouse.Zipcode, Warehouse.ID From Warehouse Where Owner_ID = ".$Owner_ID."";
+            $location_sql = "Select Warehouse.Zipcode, Warehouse.ID From Warehouse Where Owner_ID = 1";
+
+            //Makes multidimensional array of contracts_sql query result
+            $location_result = mysqli_query($conn, $location_sql);
+            $location = array();
+            if(mysqli_num_rows($location_result)>0){
+                while($row = mysqli_fetch_assoc($location_result)){
+                    $location[] = $row;
+                }
+            } 
+            $location_table = array();
+            foreach ($location as $location1){
+                $location_table[] = array((string)$location1['Zipcode'], (int)$location1['ID']); 
+            }
+
+            $location_table = json_encode($location_table);
+            
+            $conn->close(); 
+                
+        ////////Bring In Revenue Data///////////
+            /*Open Data base connection*/
+            $Owner_ID=$_SESSION["id"]; 
+            $servername = "mydb.ics.purdue.edu";
+            $username = "g1090423";
+            $password = "marioboys";
+            $dbname = "g1090423";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+
         
-       //$location_sql = "Select Warehouse.Latitude, Warehouse.Longitude, Warehouse.ID From Warehouse Where Owner_ID = ".$Owner_ID."";
-       $location_sql = "Select Warehouse.Latitude, Warehouse.Longitude, Warehouse.ID From Warehouse Where Owner_ID = 1";
+            //$contracts_sql = "Select Contract.*, Warehouse.StorageCapacity, Warehouse.BasePrice, Warehouse.Zipcode From Contract INNER JOIN Warehouse ON Contract.Warehouse_ID = Warehouse.ID Where Contract.Owner_ID = ".$Owner_ID." AND Contract.Approval=1";
+            $contracts_sql = "Select Contract.*, Warehouse.StorageCapacity, Warehouse.BasePrice, Warehouse.Zipcode From Contract INNER JOIN Warehouse ON Contract.Warehouse_ID = Warehouse.ID Where Contract.Owner_ID = 1 AND Contract.Approval=1";
+            
+            $result = mysqli_query($conn, $contracts_sql);
+            $data = array();
+            if(mysqli_num_rows($result)>0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $data[] = $row;
+                }
+            }
+        
+            
+            $revenue_table = array();
+            foreach ($data as $data1){
+                $diff = strtotime($data1['End Date']) - strtotime($data1['Start Date']);   //Site the Stack overflow
+                $diffDays = floor($diff/(3600*24));
+                $revenue = $data1['BasePrice']/365 *$data1['Rented_Space'] * $diffDays;
+                $revenue_table[] = array($data1['ID'],(int)$revenue); 
+            }
+            $revenue_table = json_encode($revenue_table);
+            $conn->close();   
+                
+        ////////Bring In Space Data///////////
+            /*Open Data base connection*/
+            $Owner_ID=$_SESSION["id"]; 
+            $servername = "mydb.ics.purdue.edu";
+            $username = "g1090423";
+            $password = "marioboys";
+            $dbname = "g1090423";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+            //Need to fix query below to be dynamic with page content before uploading
+            //$space_sql = "Select Warehouse.ID, sum(Warehouse.StorageCapacity), sum(Contract.Rented_Space) From Warehouse INNER JOIN Contract ON Warehouse.ID = Contract.Warehouse_ID Where Warehouse.Owner_ID=".$Owner_ID." AND Approval = 1";
+            $space_sql = "Select Warehouse.ID, (sum(Warehouse.StorageCapacity)- sum(Contract.Rented_Space)) as Open, sum(Contract.Rented_Space) as Rented_Space From Warehouse INNER JOIN Contract ON Warehouse.ID = Contract.Warehouse_ID Where Warehouse.Owner_ID=1 AND Approval = 1";
+            
+            $result = mysqli_query($conn, $space_sql);
+            $space = array();
+            if(mysqli_num_rows($result)>0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $space[] = $row;
+                }
+            }
+            $space_table = array();
+            $open_total = 0;
+            foreach ($space as $space1){
+                $open_total = $open_total + $space1['Open'];
+                $space_table[] =array((string)$space1['ID'], (int)$space1['Rented_Space']); 
+            }
+            
+            $open_total = array("Open", (int)$open_total);
+            $space_table[] = $open_total;
+            
+            $space_table = json_encode($space_table);
+            $conn->close();
 
-       //Makes multidimensional array of contracts_sql query result
-       $location_result = mysqli_query($conn, $location_sql);
-       $location = array();
-       if(mysqli_num_rows($location_result)>0){
-           while($row = mysqli_fetch_assoc($location_result)){
-               $location[] = $row;
-           }
-       } 
-       $location_table = array();
-       foreach ($location as $location1){
-        $location_table[] = array($location1['Latitude'],$location1['Longitude'], $location1['ID']); 
-     }
+        ////////Bring In Timeline Data///////////
+             /*Open Data base connection*/
+            $Owner_ID=$_SESSION["id"]; 
+            $servername = "mydb.ics.purdue.edu";
+            $username = "g1090423";
+            $password = "marioboys";
+            $dbname = "g1090423";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+            //Add dynamic sql
+            $query = "Select ID, UNIX_TIMESTAMP(`Start Date`) as StartDate, UNIX_TIMESTAMP(`End Date`) as EndDate FROM Contract Where Owner_ID = 1 and Approval=1";
+                $result = mysqli_query($conn, $query);
+                $rows = array();
+                $table = array();
 
-       $location_table = json_encode($location_table);
-     
-    
-        $conn->close();
+                $table['cols'] = array(
+                array(
+                'label' => 'ID', 
+                'type' => 'string'
+                ),               
+                array(
+                'label' => 'StartTime', 
+                'type' => 'datetime'
+                ),
+                array(
+                    'label' => 'EndTime', 
+                    'type' => 'datetime'
+                    ),
+                    
+                );
+
+                while($row = mysqli_fetch_array($result))
+                {
+                $sub_array = array();
+                $StartTime = explode(".", $row["StartDate"]);
+                $EndTime = explode(".", $row["EndDate"]);    
+                $sub_array[] =  array(
+                    "v" => $row["ID"]
+                    );
+                
+                $sub_array[] =  array(
+                    "v" => 'Date(' . $row["StartDate"] . '000)'
+                    );
+                $sub_array[] =  array(
+                    "v" => 'Date(' . $row["EndDate"] . '000)'
+                    );
+                $rows[] =  array(
+                    "c" => $sub_array
+                    );
+                }
+                $table['rows'] = $rows;
+                $jsonTable = json_encode($table);
+
+                
+                $conn->close();
+
+
+
+        ////Google Chart Creation/////
+            echo"<head>";
+            echo'<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>';
+            echo'<script type="text/javascript">
+            google.charts.load("visualization", "1", {packages:["map", "corechart", "timeline"], "mapsApiKey": "AIzaSyDi799MTv01g9mQ2C2p1km1v7v1bTIjs-Q"});
+            google.charts.setOnLoadCallback(init);
+
+            function init () {
+                drawMap();
+                drawRev();
+                drawSpace();
+                drawTimeline(); 
+            }
+
+            function drawMap() {
+                var dataMap = new google.visualization.DataTable();
+                    dataMap.addColumn(\'string\', \'zip\');
+                    dataMap.addColumn(\'number\', \'Warehouse ID\');
+                    dataMap.addRows('.$location_table.');
+        
+                var map = new google.visualization.Map(document.getElementById(\'map_div\'));
+                map.draw(dataMap, {
+                showTooltip: true,
+                showInfoWindow: true
+                });
+            }
+
+            function drawRev() {
+
+                var dataRev = new google.visualization.DataTable();
+                        dataRev.addColumn(\'string\', \'ID\');
+                        dataRev.addColumn(\'number\',\'Revenue\');
+                        dataRev.addRows('.$revenue_table.');
+                var options = {
+                    title: \'Percentage Revenue by Contract\',
+                    is3D: \'true\',
+                    width: 800,
+                    height: 600
+                    };
+                
+                var chartRev = new google.visualization.PieChart(document.getElementById(\'rev_div\'));
+                chartRev.draw(dataRev, options);
+                }
+            
+            function drawSpace() {
+
+                var dataSpace = new google.visualization.DataTable();
+                        dataSpace.addColumn(\'string\', \'ID\');
+                        dataSpace.addColumn(\'number\',\'Space\');
+                        dataSpace.addRows('.$space_table.');
+                var options = {
+                    title: \'Available Space\',
+                    is3D: \'true\',
+                    width: 800,
+                    height: 600
+                    };
+                
+                var chartSpace = new google.visualization.PieChart(document.getElementById(\'Space_div\'));
+                chartSpace.draw(dataSpace, options);
+                }
+
+            
+            function drawTimeline(){
+                
+                    
+                var container = document.getElementById(\'timeline\');
+                var Timeline = new google.visualization.Timeline(container);
+                var dataTime = new google.visualization.DataTable('.$jsonTable.');
+
+                var options = {
+                    title:\'Sensors Data\',
+                    legend:{position:\'bottom\'},
+                    chartArea:{width:\'95%\', height:\'65%\'}
+                    };
+
+
+                Timeline.draw(dataTime, options);
+            
+            }
+        
+            </script>';
+            echo"</head>";
+        }
     
     ?>
 
-  <head>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script type="text/javascript">
-    google.charts.load('current', {'packages':['corechart']});
-    google.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-
-      var data = new google.visualization.DataTable();
-          data.addColumn('string', 'ID');
-          data.addColumn('number','Revenue');
-          data.addRows(<?=$revenue_table?>);
-      var options = {
-          title: 'Percentage Revenue by Contract',
-          is3D: 'true',
-          width: 800,
-          height: 600
-        };
-      
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
-    </script>
-  
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script>
-    google.charts.load('current', { 'packages': ['map'] });
-    google.charts.setOnLoadCallback(drawMap);
-
-    function drawMap() {
-        var data = new google.visualization.DataTable();
-          data.addColumn('number', 'Lat');
-          data.addColumn('number','Long');
-          data.addColumn('number', 'Warehouse ID');
-          data.addRows(<?=$location_table?>);
-
-    var options = {
-      showTooltip: true,
-      showInfoWindow: true
-    };
-
-    var map = new google.visualization.Map(document.getElementById('chart_div'));
-
-    map.draw(data, options);
-  };
-  </script>
-  </head>
-
-  <body>
-    <div id="chart_div"></div>
 
   </body>
 </html>
-    
