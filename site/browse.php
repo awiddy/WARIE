c61e889b2a57e88d3e2f66757bfcba10b69f254a
sallyclemmons <44778617+sallyclemmons@users.noreply.github.com>
2018-12-03 20:41:19 -0500

    converted from html to php
    allows for correct login/logout button functionality

---
+++ site/browse.php
@@ -0,0 +1,174 @@
+<?php session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE HTML>
+<!--
+	Binary by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Browse Warehouses</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<a href="index.html" class="logo"><strong>WARIE</strong> &ensp; Home</a>
+				<nav>
+					<a href="#menu">Menu</a>
+					<a href="about.html">About</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="browse.html">Browse Warehouses</a></li>
+					<li><a href="lessees.html">Lease a warehouse</a></li>
+					<li><a href="owners.html">List your warehouse</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</nav>
+
+		<!--Banner-->
+		<section class="banner_layout banner_login">
+				<div class="inner">
+				</br></br></br>
+					<h1><font color="white">Browse Warehouses</font></h1></br>
+				</div>
+			</section>
+
+		<!-- Main -->
+			<section id="main">
+				<div class="slimmer">
+					<h3>Select your criteria to narrow the results</h3>
+					<form name="allInputs" method="post" action="http://web.ics.purdue.edu/~g1090423/results.php">
+						<div class="row uniform 50%">
+							<div class="12u$">
+								<div class="select-wrapper">
+									<h4>Warehouse Location:</h4>
+									<select name="city" required>
+										<option value="">- Location -</option>
+										<option value="San Diego">San Diego, CA</option>
+										<option value="Los Angeles">Los Angeles, CA</option>
+										<option value="San Francisco">San Francisco, CA</option>
+										<option value="San Jose">San Jose, CA</option>
+										<option value="Austin">Austin, TX</option>
+										<option value="Dallas">Dallas, TX</option>
+										<option value="Houston">Houston, TX</option>
+										<option value="Fort Worth">Fort Worth, TX</option>
+										<option value="San Antonio">San Antonio, TX</option>
+										<option value="Jacksonville">Jacksonville, FL</option>
+										<option value="Chicago">Chicago, IL</option>
+										<option value="New York">New York, NY</option>
+										<option value="Philadelphia">Philadelphia, PA</option>
+										<option value="Phoenix">Phoenix, AZ</option>
+										<option value="Columbus">Columbus, OH</option>
+									</select>
+								</div>
+							</div>
+							</br></br>
+						<div class="12u$">
+							<div class="select-wrapper">
+								<h4>Storage Environment:</h4>
+								<select name="storagetype" required>
+										<option value="">- Storage Type -</option>
+										<option value="1">General</option>
+										<option value="2">Dry</option>
+										<option value="3">Refrigerated</option>
+										<option value="4">Frozen</option>
+										</select>
+										</div>
+										</div>
+									<br><br><h4>How much space will you  be needing?</h4></br></br>
+									<div class="12u$">
+										<input type="number" min="0" name="storage_needed" id="storage_needed" value="" placeholder="Space Needed" required />
+									</div>
+									<br><br><h4>When do you need your contract to start? (Must be at least one week from today)</h4></br></br>
+									<div class="12u$">
+										<input type="date" name="start_date" id="start_date" onblur="endDate()" value="" min = "0" max="0" placeholder="Start Date" required />
+									<script>
+
+									var d = new Date().getTime() + 86400000*7; //Gets seven days from today
+									var seven_days = new Date(d).toISOString().split("T")[0]; //Convert to string
+									start_date.min = seven_days; //Set min
+									
+									</script>
+									</div>
+									<br><br><h4>When will your contract be ending? (Minimum length is one week)</h4></br></br>
+									<div class="12u$">
+										<input type="date" name="end_date" id="end_date" value="" min="0" max="0" placeholder="End Date" required />
+									</div>
+									<script>
+
+									var d = new Date().getTime() + 86400000*14; 
+									var seven_days = new Date(d).toISOString().split("T")[0]; //Same code but to initially make it 14 days from today
+									end_date.min = seven_days; 
+									
+									function endDate(){
+										
+										var d1 = new Date(start_date.value).getTime() + 86400000*7; //Add 7 days to the value of the start date
+										
+										end_date.min = new Date(d1).toISOString().split("T")[0]; //set the end date minimum to above value
+									}
+
+									</script>
+								</br></br>
+									<div class="12u$">
+									<div class="select-wrapper">
+										<h4>Sort results by:</h4>
+										<select name="sort_val" id="sort_val" required>
+											<option value="">- Sort -</option>
+											<option value="1">Price ($/sq ft/month)</option>
+											<option value="2">Rating</option>
+
+										</select>
+									</div>
+								</div>
+
+								</br></br>
+
+								<ul class="actions">
+									<li><input type="submit" value="Search" /></li>
+								</ul>
+
+
+					</div>
+				</form>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+  			<footer id="footer">
+  				 <ul class="icons">
+  					<li><a href="https://twitter.com/WARIE49834226" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+  					<li><a href="https://www.facebook.com/WARIE-639800186472059/?modal=admin_todo_tour" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+  					<li><a href="https://www.instagram.com/warie_business/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+  				</ul>
+  				<a href ="terms_conditions.html">Terms and Conditions</a><br><br>
+
+  				<div class="copyright" style="font-weight:300; font-size: 10px;">
+  					&copy; Untitled. Design: <a href="https://templated.co" style="font-weight:300;">TEMPLATED</a>. Images: <a href="https://unsplash.com" style="font-weight:300;">Unsplash</a>.
+  				</div>
+  			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ site/terms_conditions.php
@@ -0,0 +1,158 @@
+<?php session_start();?>
+
+<!DOCTYPE HTML>
+<!--
+	Binary by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Terms and Conditions</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<a href="index.html" class="logo"><strong>WARIE</strong> &ensp; Home</a>
+				<nav>
+					<a href="#menu">Menu</a>
+					<a href="about.html">About</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="browse.html">Browse Warehouses</a></li>
+					<li><a href="lessees.html">Lease a warehouse</a></li>
+					<li><a href="owners.html">List your warehouse</a></li>
+					<?php if(isset($_SESSION["loggedin"])){ 
+					echo("<li><a href='logout.php'>Logout</a></li>");
+					}
+					else{
+					echo("<li><a href='login.php'>Login</a></li>");
+					}
+					?>
+				</ul>
+			</nav>
+
+		<!--Banner-->
+		<section class="banner_layout banner_login">
+				<div class="inner">
+				</br></br></br>
+					<h1><font color="white">Terms and Conditions</font></h1></br>
+				</div>
+			</section>
+
+		<!-- Main -->
+			<section id="main">
+				<div class="slimmer">
+					<h2>Introduction</h2>
+
+<p>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions. These Terms and Conditions have been generated over at the <a href="https://termsandcondiitionssample.com">Terms And Conditions Template</a> website.</p>
+
+<p>Minors or people below 18 years old are not allowed to use this Website.</p>
+
+<h2>Intellectual Property Rights</h2>
+
+<p>Other than the content you own, under these Terms, WARIE and/or its licensors own all the intellectual property rights and materials contained in this Website.</p>
+
+<p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
+
+<h2>Restrictions</h2>
+
+<p>You are specifically restricted from all of the following:</p>
+
+<ul>
+    <li>publishing any Website material in any other media;</li>
+    <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+    <li>publicly performing and/or showing any Website material;</li>
+    <li>using this Website in any way that is or may be damaging to this Website;</li>
+    <li>using this Website in any way that impacts user access to this Website;</li>
+    <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
+    <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
+    <li>using this Website to engage in any advertising or marketing.</li>
+</ul>
+
+<p>Certain areas of this Website are restricted from being access by you and WARIE may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.</p>
+
+<h2>Your Content</h2>
+
+<p>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant WARIE a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
+
+<p>Your Content must be your own and must not be invading any third-party’s rights. WARIE reserves the right to remove any of Your Content from this Website at any time without notice.</p>
+
+<!--<h2>Your Privacy</h2>
+
+<p>Please read <a href="https://privacypolicytemplate.net/">our Privacy Policy</a>.</p>-->
+
+<h2>No warranties</h2>
+
+<p>This Website is provided "as is," with all faults, and WARIE express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
+
+<h2>Limitation of liability</h2>
+
+<p>In no event shall WARIE, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  WARIE, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
+
+<h2>Indemnification</h2>
+
+<p>You hereby indemnify to the fullest extent WARIE from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
+
+<h2>Severability</h2>
+
+<p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
+
+<h2>Variation of Terms</h2>
+
+<p>WARIE is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
+
+<h2>Assignment</h2>
+
+<p>The WARIE is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+
+<h2>Entire Agreement</h2>
+
+<p>These Terms constitute the entire agreement between WARIE and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
+
+<h2>Governing Law & Jurisdiction</h2>
+
+<p>These Terms will be governed by and interpreted in accordance with the laws of the State of us, and you submit to the non-exclusive jurisdiction of the state and federal courts located in us for the resolution of any disputes.</p>
+
+						<div class="row uniform 50%">
+						<div class="12u$">
+							<div class="select-wrapper">
+							</div>
+						</div>
+					    </div>
+
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				 <ul class="icons">
+					<li><a href="https://twitter.com/WARIE49834226" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="https://www.facebook.com/WARIE-639800186472059/?modal=admin_todo_tour" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="https://www.instagram.com/warie_business/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+				</ul>
+				<a href ="terms_conditions.html">Terms and Conditions</a><br><br>
+
+				<div class="copyright" style="font-weight:300; font-size: 10px;">
+					&copy; Untitled. Design: <a href="https://templated.co" style="font-weight:300;">TEMPLATED</a>. Images: <a href="https://unsplash.com" style="font-weight:300;">Unsplash</a>.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
